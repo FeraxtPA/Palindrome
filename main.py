432536491726be16bb923f1ab36cc28c277ecc60
FeraxtPA <feraxml@seznam.cz>
2022-10-31 21:01:16 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def palindorme(number):
+    original_number = number
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+
+    if original_number == reverse_num:
+        print("Giver number is a palindrome")
+    else:
+        print("Givern number is not a palindrome")
+
+
+number = int(input("Enter a number: "))
+print("Working")
+palindorme(number)
